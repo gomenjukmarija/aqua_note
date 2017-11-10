2c4a56d7650d9b96e92d44f8f0ecb2cdd4803047
gomenjukmarija <gomenjukmarija@gmail.com>
2017-11-10 15:29:21 +0200

    Beautiful Authentication, Powerful Authorization 16-21

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,13 +2,16 @@
 
 namespace AppBundle\Entity;
 
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\Role\Role;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="user")
+ * @UniqueEntity(fields={"email"}, message="It looks like your already have an account!")
  */
 class User implements UserInterface
 {
@@ -20,6 +23,8 @@ class User implements UserInterface
     private $id;
 
     /**
+     * @Assert\NotBlank()
+     * @Assert\Email()
      * @ORM\Column(type="string", unique=true)
      */
     private $email;
@@ -33,6 +38,7 @@ class User implements UserInterface
 
     /**
      * A non-persisted field that's used to create the encoded password.
+     * @Assert\NotBlank(groups={"Registration"})
      *
      * @var string
      */
