57ca68ae9def5331bd0db46a6244431949f164a8
gomenjukmarija <gomenjukmarija@gmail.com>
2017-11-24 10:09:22 +0200

    EasyAdminBundle for an Amazing Admin Interface 1-3

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
             new AppBundle\AppBundle(),
         ];
 
